16c964cf7f0e00c62fce44ad6c059be98f9468b9
CoolReal <1190928@isep.ipp.pt>
2023-01-14 19:56:18 +0000

    feat: added movementType and newWalletValue properties to movements

--- backend/src/model/movement.ts
+++ backend/src/model/movement.ts
@@ -1,37 +1,49 @@
 import { randomUUID } from 'crypto';
 import { currentDateAsString } from '../utils';
 
+enum MovementType {
+    INTERNAL = 'INTERNAL',
+    EXTERNAL = 'EXTERNAL',
+}
+
 export interface IMovement {
     id: string;
+    type: MovementType;
     createdAt: string;
     updatedAt: string;
     description: string;
     transactionValue: number;
+    newWalletValue: number;
     walletOriginId?: string;
     walletDestinationId?: string;
 }
 
 export class Movement implements IMovement {
     id: string;
+    type: MovementType;
     createdAt: string;
     updatedAt: string;
     description: string;
     transactionValue: number;
+    newWalletValue: number;
     walletDestinationId?: string;
     walletOriginId?: string;
 
-    constructor(
-        date: string,
+    private constructor(
         description: string,
         transactionValue: number,
-        walletDestinationId?: string,
-        walletOriginId?: string
+        newWalletValue: number,
+        type: MovementType,
+        walletOriginId?: string,
+        walletDestinationId?: string
     ) {
         this.id = randomUUID();
         this.createdAt = currentDateAsString();
         this.updatedAt = this.createdAt;
         this.description = description;
         this.transactionValue = transactionValue;
+        this.newWalletValue = newWalletValue;
+        this.type = type;
         this.walletDestinationId = walletDestinationId;
         this.walletOriginId = walletOriginId;
     }
